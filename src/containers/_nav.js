0045372523ab228cb33c10ec07b3741af85bfdce
davidsval11 <davidsval@hotmail.com>
2020-06-05 19:56:53 -0500

    first commit

---
+++ src/containers/_nav.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    _name: 'CSidebarNav',
+    _children: [
+      {
+        _name: 'CSidebarNavItem',
+        name: 'Dashboard',
+        to: '/dashboard',
+        icon: 'cil-speedometer',
+        badge: {
+          color: 'primary',
+          text: 'NEW'
+        }
+      },
+      {
+        _name: 'CSidebarNavTitle',
+        _children: ['Menu Principal']
+      },
+      {
+        _name: 'CSidebarNavItem',
+        name: 'Colors',
+        to: '/theme/colors',
+        icon: 'cil-drop'
+      },
+      {
+        _name: 'CSidebarNavDropdown',
+        name: 'Pages',
+        route: '/pages',
+        icon: 'cil-star',
+        items: [
+          {
+            name: 'Login',
+            to: '/pages/login'
+          },
+          {
+            name: 'Register',
+            to: '/pages/register'
+          },
+        ]
+      },
+    ]
+  }
+]
---
+++ src/router/index.js
@@ -0,0 +1,82 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// Containers
+const TheContainer = () => import('@/containers/TheContainer')
+
+// Views
+const Dashboard = () => import('@/views/Dashboard')
+
+const Colors = () => import('@/views/theme/Colors')
+
+const Charts = () => import('@/views/charts/Charts')
+
+
+
+// Views - Pages
+
+const Login = () => import('@/views/pages/Login')
+const Register = () => import('@/views/pages/Register')
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'hash', // https://router.vuejs.org/api/#mode
+  linkActiveClass: 'active',
+  scrollBehavior: () => ({ y: 0 }),
+  routes: configRoutes()
+})
+
+function configRoutes () {
+  return [
+    {
+      path: '/',
+      redirect: '/dashboard',
+      name: 'Home',
+      component: TheContainer,
+      children: [
+        {
+          path: 'dashboard',
+          name: 'Dashboard',
+          component: Dashboard
+        },
+        {
+          path: 'theme',
+          redirect: '/theme/colors',
+          name: 'Theme',
+          component: {
+            render (c) { return c('router-view') }
+          },
+          children: [
+            {
+              path: 'colors',
+              name: 'Colors',
+              component: Colors
+            },
+          ]
+        },
+        {
+          path: '/pages',
+          redirect: '/pages/404',
+          name: 'Pages',
+          component: {
+            render (c) { return c('router-view') }
+          },
+          children: [
+            {
+              path: 'login',
+              name: 'Login',
+              component: Login
+            },
+            {
+              path: 'register',
+              name: 'Register',
+              component: Register
+            }
+          ]
+        }
+      ] 
+    }
+  ]
+}
+
