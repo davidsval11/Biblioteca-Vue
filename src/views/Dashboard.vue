7b6e34859088806d06f232bf758f039d95601b32
Camilo <camilo@camilo-X555QG>
2020-06-06 18:09:52 -0500

    modificaciones ini camilo

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -159,19 +159,6 @@ export default {
     }
   },
   methods: {
-    color (value) {
-      let $color
-      if (value <= 25) {
-        $color = 'info'
-      } else if (value > 25 && value <= 50) {
-        $color = 'success'
-      } else if (value > 50 && value <= 75) {
-        $color = 'warning'
-      } else if (value > 75 && value <= 100) {
-        $color = 'danger'
-      }
-      return $color
-    }
   }
 }
 </script>
